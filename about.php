2494ed53f5a0cbc52c566f5589976518d7d09220
bexfisher <pixieartistbex@hotmail.com>
2017-01-22 11:41:48 +0000

    Start Here
    Initial blank template for Bex Fisher site

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php include 'header.php' ?>
+
+   
+   
+    <div class="container">
+        <br><br><br>
+        <h1>About</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.
+            <br> All you get is this text and a mostly barebones HTML document.</p>
+    </div>
+    <!-- /.container -->
+    
+    
+    
+  
+
+    
+   <?php include 'footer.php' ?>
